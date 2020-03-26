d6a88ce407df3ca5785dd810011c2cbd6da21eb2
Aidan Reid <aidan.reid@hotmail.com>
2020-03-26 16:27:40 +0000

    narrowed text on cards to make text readable, added img from canva and finished book app

--- js/book.js
+++ js/book.js
@@ -19,19 +19,46 @@ UI.prototype.addBookToList = function(book){
     <td>${book.author}</td>
     <td>${book.isbn}</td>
     <td><a href="#" class="delete">X</a></td>
-    `
+    `;
 
     list.appendChild(row);
+}
 
+    // Show alerts
+    UI.prototype.showAlert = function(message, className){
+        // Create Div
+        const div = document.createElement("div");
+        // Add classes
+        div.className = `alert ${className}`;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector(".container");
+        const form = document.querySelector("#book-form");
+        // Insert alert
+        container.insertBefore(div, form);
+
+        // Timeout after three seconds
+        setTimeout(function(){
+            document.querySelector(".alert").remove();
+        }, 3000);
+    }
+
+    // Delete Book
+    UI.prototype.deleteBook = function(target){
+        if(target.className === "delete"){
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    // Clear Fields
     UI.prototype.clearFields = function (){
         document.getElementById("title").value = "";
         document.getElementById("author").value = "";
         document.getElementById("isbn").value = "";
     }
 
-}
-
-// Event Listeners
+// Event Listener for Add Book
 document.getElementById("book-form").addEventListener("submit", function(e){
     // Get form values
     const title = document.getElementById("title").value,
@@ -44,11 +71,33 @@ document.getElementById("book-form").addEventListener("submit", function(e){
     // Instantiate UI Object
     const ui = new UI();
 
-    // Add Book To List
-    ui.addBookToList(book);
+    // Validate
+    if (title === "" || author === "" || isbn === ""){
+        // Error
+        ui.showAlert("Fill in ALL fields dipshit!", "error");
+    } else {
+        // Add Book To List
+        ui.addBookToList(book);
 
-    // CLear Fields
-    ui.clearFields();
+        //Show success
+        ui.showAlert("Yay! Book added", "success");
+
+        // CLear Fields
+        ui.clearFields();
+        }
+
+    e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById("book-list").addEventListener("click", function(e){
+    
+    // Instantiate UI Object
+    const ui = new UI();
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert("Book Removed", "success");
 
     e.preventDefault();
 });
