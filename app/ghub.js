f5bad410bd5df2c25aca5bda63e8c0c650ec3e98
Aidan Reid <aidan.reid@hotmail.com>
2020-05-13 13:16:57 +0000

    github searcher app completed

--- app/ghub.js
+++ app/ghub.js
@@ -1,16 +1,22 @@
 class Github {
-    constuctor(){
+    constructor(){
         this.client_id = '186a13c8cd8f0cc5d741';
         this.client_secret = '08f5eced68eb67b3a135837a2d4ede4ac938314f';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
