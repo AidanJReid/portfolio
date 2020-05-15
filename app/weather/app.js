dd2d4bbc7a157dd2c15074265387f2da035ded45
Aidan Reid <aidan.reid@hotmail.com>
2020-05-15 15:22:54 +0000

    final changes to weather app

--- app/weather/app.js
+++ app/weather/app.js
@@ -1,11 +1,35 @@
+// Init Storage
+const storage = new Storage();
+
+// Get Stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather('Dublin', 'Ireland');
+const weather = new Weather(weatherLocation.city);
 
 // Init UI
 const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-changeButton').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    
+    // Change location
+    weather.changeLocation(city);
+
+    // Set location in local storage
+    storage.setLocationData(city)
+
+    // Get and display weather
+    getWeather();
+
+    // Close modal
+    $('#locModal').modal('hide');
+});
+
 
 function getWeather(){
     weather.getWeather()
--- app/weather/weather.js
+++ app/weather/weather.js
@@ -1,13 +1,12 @@
 class Weather {
-  constructor(city, country){
+  constructor(city){
       this.apiKey = '30b7b9cc0f40f4d5ef576b370b498e90';
       this.city = city;
-      this.country = country;
   }
 
   // Fetch weather from API
   async getWeather() {
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric`);
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`);
 
       const responseData = await response.json();
 
@@ -16,8 +15,7 @@ class Weather {
   }
 
   // Change weather location
-  changeLocation(city, country) {
+  changeLocation(city) {
       this.city = city;
-      this.country = country;
   }
 }
